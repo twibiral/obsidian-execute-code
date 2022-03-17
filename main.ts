405910390d756e0b809c182110eebefc86c33ed2
Tim Wibiral <tim.wibiral@gmail.com>
2022-03-17 21:04:29 +0100

    first commit

--- main.ts
+++ main.ts
@@ -1,137 +1,110 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { ChildProcess } from 'child_process';
+import { V4MAPPED } from 'dns';
+import { App, Editor, MarkdownView, Modal, Notice, parseFrontMatterAliases, Plugin, Workspace, WorkspaceParent, WorkspaceSidedock} from 'obsidian';
+import { Context, createContext, Script } from 'vm';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+const supportedLanguages = ["js"];
+const vmContextOptions = {
+	filename: 'obsidian-note.js', // filename for stack traces
+	lineOffset: 1, // line number offset to be used for stack traces
+	columnOffset: 1, // column number offset to be used for stack traces
+	displayErrors: true,
+	timeout: 1000, // ms
+	require: require,
+	console: console
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const buttonText = "Run";
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const runButtonClass = "run-code-button";
+const hasButtonClass = "has-run-code-button";
 
+export default class ExecuteCodePlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		console.log("loading plugin: Execute Code");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.injectButtons();
+		this.registerInterval(window.setInterval(this.injectButtons.bind(this), 2000));
 	}
 
 	onunload() {
+		document
+			.querySelectorAll("pre > code")
+			.forEach((codeBlock: HTMLElement) => {
+				const pre = codeBlock.parentElement as HTMLPreElement;
+				const parent = pre.parentElement as HTMLDivElement;
 
+				if(parent.hasClass(hasButtonClass)){
+					// remove existing button
+					parent.removeClass(hasButtonClass);
+				}
+			});
+
+		document
+			.querySelectorAll("pre button")
+			.forEach((button: HTMLButtonElement) => {		
+				if(button.classList.contains(runButtonClass)) {
+					button.remove();
+				}
+			})
+
+		console.log("unloaded plugin: Execute Code");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	injectButtons() {
+		this.addRunButtons();
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	addRunButtons() {
+		document
+			.querySelectorAll("pre > code")
+			.forEach((codeBlock: HTMLElement) => {
+				const pre = codeBlock.parentElement as HTMLPreElement;
+				const parent = pre.parentElement as HTMLDivElement;
+				const language = pre.className.toLowerCase();
+
+				if(! supportedLanguages.some(
+					(lang) =>pre.classList.contains(`language-${lang}`)
+				)) {
+					// Unsupported language
+					return 0;
+				}
+
+				if(parent.hasClass(hasButtonClass)){
+					// Already has a button
+					return 0;
+				}
+
+				// Add button:
+				if(language.contains("language-js")) {
+					parent.classList.add(hasButtonClass);
+					console.log("Add run button");
+
+					const button = document.createElement("button");
+					button.className = runButtonClass;
+					button.setText(buttonText);
+
+					pre.appendChild(button);
+					button.addEventListener("click", () => this.runJavaScript(codeBlock.innerText));
+				}
+			})
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	runJavaScript(codeBlockContent: string) {
+		new Notice("Running...")
+				
+		const sandbox = {__history__: new Array<string>()};
+		const newCons = "var console = {};console.log=(x)=>__history__.push(x);"
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		try {
+			const script = new Script(newCons + codeBlockContent);
+			script.runInNewContext(sandbox, vmContextOptions);
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		} catch(err) {
+			console.log(err);
+		}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		console.log(sandbox);
 	}
 }
