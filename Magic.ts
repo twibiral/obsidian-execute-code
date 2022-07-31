26266df274a7986d843b378a016b8dacc9a931b5
Tim Wibiral <tim.wibiral.tw@gmail.com>
2022-07-31 16:53:10 +0200

    feat: add "magic" that returns the title of the current note as string

--- Magic.ts
+++ Magic.ts
@@ -5,11 +5,15 @@
  * - `@show(ImagePath)`: Displays an image at the given path in the note.
  * - `@show(ImagePath, Width, Height)`: Displays an image at the given path in the note.
  * - `@show(ImagePath, Width, Height, Alignment)`: Displays an image at the given path in the note.
+ * - `@vault`: Inserts the vault path as string.
+ * - `@note`: Inserts the note path as string.
+ * - `@title`: Inserts the note title as string.
  */
 
 const SHOW_REGEX = /@show\(["'](?<path>[^<>?*=!\n#()\[\]{}]+)["'](,\s*(?<width>[0-9]+[\w%]+),?\s*(?<height>[0-9]+[\w%]+))?(,\s*(?<align>left|center|right))?\)/g;
 const VAULT_REGEX = /@vault/g
 const CURRENT_NOTE_REGEX = /@note/g;
+const NOTE_TITLE_REGEX = /@title/g;
 
 export function insertVaultPath(source: string, vaultPath: string): string {
 	return source.replace(VAULT_REGEX, `"app://local/${vaultPath.replace(/\\/g, "/")}"`);
@@ -17,6 +21,10 @@ export function insertVaultPath(source: string, vaultPath: string): string {
 
 export function insertNotePath(source: string, notePath: string): string {
 	return source.replace(CURRENT_NOTE_REGEX, `"${notePath}"`);
+}
+
+export function insertNoteTitle(source: string, noteTitle: string): string {
+	return source.replace(NOTE_TITLE_REGEX, `"${noteTitle}"`);
 }
 
 export function addInlinePlotsToPython(source: string): string {
