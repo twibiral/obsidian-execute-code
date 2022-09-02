944636beddfd6334a351d7d7c691fca957fcaba6
Tim Wibiral <tim.wibiral.tw@gmail.com>
2022-09-02 15:44:37 +0200

    feat: add settings for powershell

--- SettingsTab.ts
+++ SettingsTab.ts
@@ -206,6 +206,40 @@ export class SettingsTab extends PluginSettingTab {
 				}));
 
 
+		// ========== Powershell ==========
+		containerEl.createEl('h3', {text: 'Powershell Settings'});
+		new Setting(containerEl)
+			.setName('Powershell path')
+			.setDesc('The path to Powershell.')
+			.addText(text => text
+				.setValue(this.plugin.settings.powershellPath)
+				.onChange(async (value) => {
+					let sanitized = this.sanitizePath(value);
+					this.plugin.settings.powershellPath = sanitized;
+					console.log('Powershell path set to: ' + sanitized);
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Shell arguments')
+			.addText(text => text
+				.setValue(this.plugin.settings.powershellArgs)
+				.onChange(async (value) => {
+					this.plugin.settings.powershellArgs = value;
+					console.log('Powershell args set to: ' + value);
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Shell file extension')
+			.setDesc('Changes the file extension for generated shell scripts. This is useful if you want to use a shell other than bash.')
+			.addText(text => text
+				.setValue(this.plugin.settings.powershellFileExtension)
+				.onChange(async (value) => {
+					this.plugin.settings.powershellFileExtension = value;
+					console.log('Powershell file extension set to: ' + value);
+					await this.plugin.saveSettings();
+				}));
+
+
 		// ========== Prolog ==========
 		containerEl.createEl('h3', {text: 'Prolog Settings'});
 		new Setting(containerEl)
