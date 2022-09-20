7aa5999eaaa957729a3cb02b7660a74284c8bc10
Tim Wibiral <tim.wibiral@gmail.com>
2022-09-20 18:27:23 +0200

    refactor: move default settings to its own file

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ import {
 } from "./Magic";
 // @ts-ignore
 import * as prolog from "tau-prolog";
+import {DEFAULT_SETTINGS} from "./Settings";
 
 export const supportedLanguages = ["js", "javascript", "python", "cpp", "prolog", "shell", "bash", "groovy", "r", "go", "rust",
 	"java", "powershell", "kotlin"] as const;
@@ -26,56 +27,6 @@ const buttonText = "Run";
 const runButtonClass = "run-code-button";
 const runButtonDisabledClass = "run-button-disabled";
 const hasButtonClass = "has-run-code-button";
-
-const DEFAULT_SETTINGS: ExecutorSettings = {
-	timeout: 10000,
-	nodePath: "node",
-	nodeArgs: "",
-	jsInject: "",
-	pythonPath: "python",
-	pythonArgs: "",
-	pythonEmbedPlots: true,
-	pythonInject: "",
-	shellPath: "bash",
-	shellArgs: "",
-	shellFileExtension: "sh",
-	shellInject: "",
-	groovyPath: "groovy",
-	groovyArgs: "",
-	groovyFileExtension: "groovy",
-	groovyInject: "",
-	golangPath: "go",
-	golangArgs: "run",
-	golangFileExtension: "go",
-	goInject: "",
-	javaPath: "java",
-	javaArgs: "-ea",
-	javaFileExtension: "java",
-	javaInject: "",
-	maxPrologAnswers: 15,
-	prologInject: "",
-	powershellPath: "powershell",
-	powershellArgs: "-file",
-	powershellFileExtension: "ps1",
-	powershellInject: "",
-	cargoPath: "cargo",
-	cargoArgs: "run",
-	rustInject: "",
-	cppRunner: "cling",
-	cppInject: "",
-	clingPath: "cling",
-	clingArgs: "",
-	clingStd: "c++17",
-	rustFileExtension: "rs",
-	RPath: "Rscript",
-	RArgs: "",
-	REmbedPlots: true,
-	rInject: "",
-	kotlinPath: "kotlinc",
-	kotlinArgs: "-script",
-	kotlinFileExtension: "kts",
-	kotlinInject: "",
-}
 
 export default class ExecuteCodePlugin extends Plugin {
 	settings: ExecutorSettings;
