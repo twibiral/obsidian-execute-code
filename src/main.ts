7f5600a1b153c9ece97c7639a23f751adacc7fe8
Tim Wibiral <tim.wibiral@gmail.com>
2022-09-21 15:03:38 +0200

    docu: add more and better documentation to the functions

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,9 @@ const hasButtonClass = "has-run-code-button";
 export default class ExecuteCodePlugin extends Plugin {
 	settings: ExecutorSettings;
 
+	/**
+	 * Preparations for the plugin (adding buttons, html elements and event listeners).
+	 */
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new SettingsTab(this.app, this));
@@ -54,6 +57,9 @@ export default class ExecuteCodePlugin extends Plugin {
 		})
 	}
 
+	/**
+	 *  Remove all generated html elements (run & clear buttons, output elements) when the plugin is disabled.
+	 */
 	onunload() {
 		document
 			.querySelectorAll("pre > code")
@@ -85,15 +91,26 @@ export default class ExecuteCodePlugin extends Plugin {
 		console.log("Unloaded plugin: Execute Code");
 	}
 
+	/**
+	 * Loads the settings for this plugin from the corresponding save file and stores them in {@link settings}.
+	 */
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
+	/**
+	 * Saves the settings in {@link settings} to the corresponding save file.
+	 */
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 
-	// Perform magic on source code to insert title etc
+	/**
+	 * Perform magic on source code (parse the magic commands) to insert note path, title, vault path, etc.
+	 *
+	 * @param srcCode Code with magic commands.
+	 * @returns The input code with magic commands replaced.
+	 */
 	private transformCode(srcCode: string) {
 		let ret = srcCode;
 		const vars = this.getVaultVariables();
@@ -107,18 +124,30 @@ export default class ExecuteCodePlugin extends Plugin {
 		return ret;
 	}
 
-	// Transform a language name, to enable working with multiple language aliases, for example
-	private transformLanguage(language: string) {
+	/**
+	 * Transform a language name, to enable working with multiple language aliases, for example "js" and "javascript".
+	 *
+	 * @param language A language name or shortcut (e.g. 'js', 'python' or 'shell')
+	 * @returns The same language shortcut for every alias of the language.
+	 */
+	private getLanguageAlias(language: string) {
 		return language
 			.replace("javascript", "js")
 			.replace("bash", "shell");
 	}
 
+	/**
+	 * Takes the source code of a code block and adds all relevant pre-/post-blocks and global code injections.
+	 *
+	 * @param srcCode The source code of the code block.
+	 * @param _language The language of the code block.
+	 * @returns
+	 */
 	private async injectCode(srcCode: string, _language: ExecutorSettingsLanguages) {
 		let prependSrcCode = "";
 		let appendSrcCode = "";
 
-		const language = this.transformLanguage(_language);
+		const language = this.getLanguageAlias(_language);
 		const preLangName = `pre-${language}`;
 		const postLangName = `post-${language}`;
 
@@ -148,7 +177,7 @@ export default class ExecuteCodePlugin extends Plugin {
 					currentCode = "";
 					insideCodeBlock = false;
 				} else {
-					currentLanguage = this.transformLanguage(line.split("```")[1].trim().split(" ")[0]);
+					currentLanguage = this.getLanguageAlias(line.split("```")[1].trim().split(" ")[0]);
 					// Don't check code blocks from a different language
 					isLanguageEqual = /[^-]*$/.exec(language)[0] === /[^-]*$/.exec(currentLanguage)[0];
 					insideCodeBlock = true;
@@ -163,6 +192,12 @@ export default class ExecuteCodePlugin extends Plugin {
 		return this.transformCode(injectedCode);
 	}
 
+	/**
+	 * Add a button to each code block that allows the user to run the code. The button is only added if the code block
+	 * utilizes a language that is supported by this plugin.
+	 *
+	 * @param element The parent element (i.e. the currently showed html page / note).
+	 */
 	private addRunButtons(element: HTMLElement) {
 		element.querySelectorAll("code")
 			.forEach((codeBlock: HTMLElement) => {
@@ -186,6 +221,15 @@ export default class ExecuteCodePlugin extends Plugin {
 			});
 	}
 
+	/**
+	 * Add a listener to the run button that executes the code block on click.
+	 * Adds a different kind of listener for each supported language.
+	 *
+	 * @param language The programming language used in the code block.
+	 * @param srcCode The code in the code block.
+	 * @param button The button element to which the listener is added.
+	 * @param out The {@link Outputter} object that is used to display the output of the code.
+	 */
 	private addListenerToButton(language: string, srcCode: string, button: HTMLButtonElement, out: Outputter) {
 		if (language.contains("language-js") || language.contains("language-javascript")) {
 			button.addEventListener("click", async () => {
@@ -245,7 +289,7 @@ export default class ExecuteCodePlugin extends Plugin {
 				const prologCode = (await this.injectCode(srcCode, "prolog")).split(/\n+%+\s*query\n+/);
 				if (prologCode.length < 2) return;	// no query found
 
-				this.runPrologCode(prologCode, out);
+				this.runPrologCode(prologCode[0], prologCode[1], out);
 
 				button.className = runButtonClass;
 			});
@@ -290,7 +334,14 @@ export default class ExecuteCodePlugin extends Plugin {
 		}
 	}
 
-	private getVaultVariables() {
+	/**
+	 * Tries to get the active view from obsidian and returns a dictionary containing the file name, folder path,
+	 * file path, and vault path of the currently opened / focused note.
+	 *
+	 * @returns { fileName: string; folder: string; filePath: string; vaultPath: string } A dictionary containing the
+	 * file name, folder path, file path, and vault path of the currently opened / focused note.
+	 */
+	private getVaultVariables(): { fileName: string; folder: string; filePath: string; vaultPath: string } {
 		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (activeView == null) {
 			return null;
@@ -310,6 +361,11 @@ export default class ExecuteCodePlugin extends Plugin {
 		}
 	}
 
+	/**
+	 * Creates a new run button and returns it.
+	 *
+	 * @returns { HTMLButtonElement } The newly created run button.
+	 */
 	private createRunButton() {
 		console.debug("Add run button");
 		const button = document.createElement("button");
@@ -318,11 +374,29 @@ export default class ExecuteCodePlugin extends Plugin {
 		return button;
 	}
 
+	/**
+	 * Creates a new unique file name for the given file extension. The file path is set to the temp path of the os.
+	 * The file name is the current timestamp: '/{temp_dir}/temp_{timestamp}.{file_extension}'
+	 *
+	 * @param ext The file extension. Should correspond to the language of the code.
+	 * @returns [string, number] The file path and the file name without extension.
+	 */
 	private getTempFile(ext: string): [string, number] {
 		const now = Date.now();
 		return [`${os.tmpdir()}/temp_${now}.${ext}`, now];
 	}
 
+	/**
+	 * Creates new Notice that is displayed in the top right corner for a few seconds and contains an error message.
+	 * Additionally, the error is logged to the console and showed in the output panel ({@link Outputter}).
+	 *
+	 * @param cmd The command that was executed.
+	 * @param cmdArgs The arguments that were passed to the command.
+	 * @param tempFileName The name of the temporary file that contained the code.
+	 * @param err The error that was thrown.
+	 * @param outputter The outputter that should be used to display the error.
+	 * @private
+	 */
 	private notifyError(cmd: string, cmdArgs: string, tempFileName: string, err: any, outputter: Outputter) {
 		const errorMSG = `Error while executing ${cmd} ${cmdArgs} ${tempFileName}: ${err}`
 		console.error(errorMSG);
@@ -330,6 +404,19 @@ export default class ExecuteCodePlugin extends Plugin {
 		new Notice("Error while executing code!");
 	}
 
+	/**
+	 * Executes the code with the given command and arguments. The code is written to a temporary file and then executed.
+	 * The output of the code is displayed in the output panel ({@link Outputter}).
+	 * If the code execution fails, an error message is displayed and logged.
+	 * After the code execution, the temporary file is deleted and the run button is re-enabled.
+	 *
+	 * @param codeBlockContent The content of the code block that should be executed.
+	 * @param outputter The {@link Outputter} that should be used to display the output of the code.
+	 * @param button The button that was clicked to execute the code. Is re-enabled after the code execution.
+	 * @param cmd The command that should be used to execute the code. (e.g. python, java, ...)
+	 * @param cmdArgs Additional arguments that should be passed to the command.
+	 * @param ext The file extension of the temporary file. Should correspond to the language of the code. (e.g. py, ...)
+	 */
 	private runCode(codeBlockContent: string, outputter: Outputter, button: HTMLButtonElement, cmd: string, cmdArgs: string, ext: string) {
 		new Notice("Running...");
 		const [tempFileName, fileId] = this.getTempFile(ext)
@@ -352,6 +439,17 @@ export default class ExecuteCodePlugin extends Plugin {
 			});
 	}
 
+	/**
+	 * Executes the code with the given command and arguments. The code is written to a temporary file and then executed.
+	 * This is equal to {@link runCode} but the code is executed in a shell. This is necessary for some languages like groovy.
+	 *
+	 * @param codeBlockContent The content of the code block that should be executed.
+	 * @param outputter The {@link Outputter} that should be used to display the output of the code.
+	 * @param button The button that was clicked to execute the code. Is re-enabled after the code execution.
+	 * @param cmd The command that should be used to execute the code. (e.g. python, java, ...)
+	 * @param cmdArgs Additional arguments that should be passed to the command.
+	 * @param ext The file extension of the temporary file. Should correspond to the language of the code. (e.g. py, ...)
+	 */
 	private runCodeInShell(codeBlockContent: string, outputter: Outputter, button: HTMLButtonElement, cmd: string, cmdArgs: string, ext: string) {
 		new Notice("Running...");
 		const [tempFileName] = this.getTempFile(ext)
@@ -373,13 +471,21 @@ export default class ExecuteCodePlugin extends Plugin {
 			});
 	}
 
-	private runPrologCode(prologCode: string[], out: Outputter) {
+	/**
+	 * Executes a string with prolog code using the TauProlog interpreter.
+	 * All queries must be below a line containing only '% queries'.
+	 *
+	 * @param facts Contains the facts.
+	 * @param queries Contains the queries.
+	 * @param out The {@link Outputter} that should be used to display the output of the code.
+	 */
+	private runPrologCode(facts: string, queries: string, out: Outputter) {
 		new Notice("Running...");
 		const session = prolog.create();
-		session.consult(prologCode[0]
+		session.consult(facts
 			, {
 				success: () => {
-					session.query(prologCode[1]
+					session.query(queries
 						, {
 							success: async (goal: any) => {
 								console.debug(`Prolog goal: ${goal}`)
@@ -390,7 +496,7 @@ export default class ExecuteCodePlugin extends Plugin {
 									await session.answer({
 										success: function (answer: any) {
 											new Notice("Done!");
-											console.debug(`Prolog result:${session.format_answer(answer)}`);
+											console.debug(`Prolog result: ${session.format_answer(answer)}`);
 											out.write(session.format_answer(answer) + "\n");
 										},
 										fail: function () {
@@ -426,6 +532,15 @@ export default class ExecuteCodePlugin extends Plugin {
 		);
 	}
 
+	/**
+	 * Handles the output of a child process and redirects stdout and stderr to the given {@link Outputter} element.
+	 * Removes the temporary file after the code execution. Creates a new Notice after the code execution.
+	 *
+	 * @param child The child process to handled.
+	 * @param outputter The {@link Outputter} that should be used to display the output of the code.
+	 * @param button The button that was clicked to execute the code. Is re-enabled after the code execution.
+	 * @param fileName The name of the temporary file that was created for the code execution.
+	 */
 	private handleChildOutput(child: child_process.ChildProcessWithoutNullStreams, outputter: Outputter, button: HTMLButtonElement, fileName: string) {
 		outputter.clear();
 
