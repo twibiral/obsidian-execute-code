7c835cc7247e752b7ce9e1511a16d41e9838a302
chlohal <coleh@coleh.net>
2022-09-28 11:37:07 -0400

    fix:Refactor into a sigil-based 'block is done' detection method, which is more rigorous

--- src/executors/PythonExecutor.ts
+++ src/executors/PythonExecutor.ts
@@ -43,7 +43,10 @@ export default class PythonExecutor extends AsyncExecutor {
 
     async run(code: string, outputter: Outputter, cmd: string, cmdArgs: string, ext: string) {
         return this.addJobToQueue((resolve, reject) => {
-            this.process.stdin.write(code + "\n");
+            const finishSigil = `SIGIL_BLOCK_DONE${Math.random()}_${Date.now()}_${code.length}`;
+            
+            //import print from builtins to circumnavigate the case where the user redefines print
+            this.process.stdin.write(code + `\n\nfrom builtins import print\nprint("${finishSigil}", end = '')\n`);
             
             outputter.clear();
             
@@ -52,27 +55,28 @@ export default class PythonExecutor extends AsyncExecutor {
             });
 
             let writeToStdout = (data: any) => {
-                console.log(data);
-                outputter.write(data.toString());
+                let str = data.toString();
+                console.log(JSON.stringify(str), finishSigil);
+                
+                if (str.endsWith(finishSigil)) {
+                    console.log(JSON.stringify(str));
+                    str = str.substring(0, str.length - finishSigil.length);
+                    console.log(str);
+                    
+                    this.process.stdout.removeListener("data", writeToStdout)
+                    this.process.stderr.removeListener("data", writeToStderr);
+                    outputter.write(str);
+                    
+                    resolve();
+                } else {
+                    outputter.write(str);
+                }
             };
 
             let writeToStderr = (data: any) => {
-                const stringData = data.toString();
-                
-                console.log(data);
-
-                const removedPrompts = stringData.replace(/(^((\.\.\.|>>>) )+)|(((\.\.\.|>>>) )+$)/g, "")
+                const removedPrompts = data.toString().replace(/(^((\.\.\.|>>>) )+)|(((\.\.\.|>>>) )+$)/g, "")
 
                 outputter.writeErr(removedPrompts);
-
-                if (stringData.endsWith(">>> ")) {
-                    //wait a little bit for the stdout to flush
-                    setTimeout(() => {
-                        this.process.stdout.removeListener("data", writeToStdout)
-                        this.process.stderr.removeListener("data", writeToStderr);
-                        resolve();
-                    }, 200);
-                }
             }
 
             this.process.stdout.on("data", writeToStdout);
