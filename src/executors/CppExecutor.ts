1f5437870c1945011da6f7bbedee263dbd0e53fc
Milan <milan.338@outlook.com>
2022-10-07 23:47:01 +1030

    docu: document cpp exector handleChildLogic method

--- src/executors/CppExecutor.ts
+++ src/executors/CppExecutor.ts
@@ -31,14 +31,22 @@ export default class CppExecutor extends NonInteractiveCodeExecutor {
 		await this.handleChildOutput(child, outputter, undefined);
 	}
 
+	/**
+	 * Run parent NonInteractiveCodeExecutor handleChildOutput logic, but replace temporary main function name
+	 * In all outputs from stdout and stderr callbacks, from temp_<id>() to main() to produce understandable output
+	 */
 	override async handleChildOutput(child: ChildProcessWithoutNullStreams, outputter: Outputter, fileName: string) {		
 		super.handleChildOutput(child, outputter, fileName);
+		// Remove existing stdout and stderr callbacks
 		child.stdout.removeListener("data", this.stdoutCb);
 		child.stderr.removeListener("data", this.stderrCb);
 		const fileId = this.tempFileId;
+		// Replace temp_<id>() with main()
 		const replaceTmpId = (data: string) => {
 			return data.replace(new RegExp(`temp_${fileId}\\(\\)`, "g"), "main()");
 		}
+		// Set new stdout and stderr callbacks, the same as in the parent,
+		// But replacing temp_<id>() with main()
 		child.stdout.on("data", (data) => {
 			this.stdoutCb(replaceTmpId(data.toString()));
 		});
