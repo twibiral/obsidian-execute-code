9204faee9540d397383d270f4bd24c5fcead9415
chlohal <coleh@coleh.net>
2022-10-12 09:28:20 -0400

    fix: use circle elements instead of browser-unsupported weird svg

--- src/svgs/loadEllipses.ts
+++ src/svgs/loadEllipses.ts
@@ -1,7 +1,9 @@
 import parseHTML from "./parseHTML";
 
 const svg = parseHTML(`<svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 5 V 5 M 5 5 V 5 M 9 5 V 5" style="fill: none; stroke: currentColor; stroke-width: 0.5"/>
+        <circle cx="1.5" r="1" cy="5" style="fill:currentColor"/>
+        <circle cx="5" r="1" cy="5" style="fill:currentColor"/>
+        <circle cx="8.5" r="1" cy="5" style="fill:currentColor"/>
         </svg>`);
 
 export default () => {
