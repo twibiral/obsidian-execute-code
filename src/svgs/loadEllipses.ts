692fc96599a121c4d42176fc635d85ff0549afd6
chlohal <coleh@coleh.net>
2022-10-13 11:11:45 -0400

    feat: animated queuing indicator
    Co-Authored-By: Milan <milan.338@outlook.com>

--- src/svgs/loadEllipses.ts
+++ src/svgs/loadEllipses.ts
@@ -1,10 +1,17 @@
 import parseHTML from "./parseHTML";
 
 const svg = parseHTML(`<svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="1.5" r="1" cy="5" style="fill:currentColor"/>
-        <circle cx="5" r="1" cy="5" style="fill:currentColor"/>
-        <circle cx="8.5" r="1" cy="5" style="fill:currentColor"/>
-        </svg>`);
+    <style>
+        @keyframes load_ellipse_anim{
+	    0%{transform: translateY(0);}
+	    25%{transform: translateY(-1.5px);}
+	    100%{transform: translateY(0);}
+        }
+    </style>
+    <circle cx="1.5" r="1" cy="5" style="fill:currentColor; animation: load_ellipse_anim 1.3s infinite ease-in-out 0.3s;"/>
+    <circle cx="5" r="1" cy="5" style="fill:currentColor; animation: load_ellipse_anim 1.3s infinite ease-in-out 0.6s;"/>
+    <circle cx="8.5" r="1" cy="5" style="fill:currentColor; animation: load_ellipse_anim 1.3s infinite ease-in-out 0.9s;"/>
+</svg>`);
 
 export default () => {
     return svg.cloneNode(true);
