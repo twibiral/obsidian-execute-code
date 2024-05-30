e290b7f7ed23c9ff8d3a418195bce1329cb96d18
James Tan <32316305+jamesbtan@users.noreply.github.com>
2024-05-30 01:43:13 -0500

    Prevent killWithChildren from needless throwing
    - Does not throw on a 1 error-code which signifies a non-error state

--- src/executors/killWithChildren.ts
+++ src/executors/killWithChildren.ts
@@ -4,7 +4,14 @@ export default (pid: number) => {
     if(process.platform === "win32") {
         execSync(`taskkill /pid ${pid} /T /F`)
     } else {
-        execSync(`pkill -P ${pid}`)
+		try {
+	        execSync(`pkill -P ${pid}`)
+		} catch(err) {
+			// An error code of 1 signifies that no children were found to kill
+			// In this case, ignore the error
+			// Otherwise, re-throw it.
+			if (err.status !== 1) throw err;
+		}
         process.kill(pid);
     }
 }
